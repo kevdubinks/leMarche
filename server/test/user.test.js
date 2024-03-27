8928d7a9fd7363ce409009a19ae7f9fc5fe65e85
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-03-27 22:13:19 +0100

    up

---
+++ server/test/user.test.js
@@ -0,0 +1,18 @@
+// tests/User.test.js
+const User = require("../models/User");
+
+describe("User Model", () => {
+  it("should hash the password before saving", async () => {
+    const userData = {
+      username: "testUser",
+      email: "test@example.com",
+      password: "password",
+    };
+    const user = new User(userData);
+
+    await user.save();
+
+    expect(user.password).not.toBe(userData.password);
+    // Assurez-vous que la logique de hashage est implémentée dans votre modèle User
+  });
+});
