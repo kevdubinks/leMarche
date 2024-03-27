e1e64234dc6feb1c4dcda6bb0c5cf1a995f884de
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-03-27 22:07:50 +0100

    up

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-
+const rateLimit = require("express-rate-limit");
 // Importation de la configuration de la base de données
 require("./database");
-
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limite chaque IP à 100 requêtes par `window` (ici, par 15 minutes)
+  standardHeaders: true, // Retourne les infos du rate limit dans les headers `RateLimit-*`
+  legacyHeaders: false, // Désactive les headers `X-RateLimit-*`
+});
+app.use(limiter);
 app.get("/", (req, res) => {
   res.send("LeMarché.fr est en ligne !");
 });
