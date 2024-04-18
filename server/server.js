90b2de991d0452160efaf5e790ec9b5cd34b6225
ndoyeKevin <kevin.ndoye@institutsolacroup.com>
2024-04-18 11:38:15 +0200

    up

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const rateLimit = require("express-rate-limit");
+const apiRoute = require("./routes/apiRoutes");
 // Importation de la configuration de la base de données
 require("./database");
 const limiter = rateLimit({
@@ -14,7 +15,7 @@ app.use(limiter);
 app.get("/", (req, res) => {
   res.send("LeMarché.fr est en ligne !");
 });
-
+app.use("/api", apiRoutes);
 app.listen(port, () => {
   console.log(`LeMarché.fr écoutant sur le port ${port}`);
 });
